58ef3a2b6833f71635488aa707fa2548c232c512
YS <87403433+AndyChoi4495@users.noreply.github.com>
2024-04-15 23:15:59 -0400

    Update firebaseConfig.js

--- Owner/firebaseConfig.js
+++ Owner/firebaseConfig.js
@@ -8,14 +8,8 @@ import { getStorage } from 'firebase/storage';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: 'AIzaSyD4rlSqFrPwqIbQxLtBhYd8m0wuBvhQ4xI',
-    authDomain: 'react-native-ys.firebaseapp.com',
-    projectId: 'react-native-ys',
-    storageBucket: 'react-native-ys.appspot.com',
-    messagingSenderId: '555653275228',
-    appId: '1:555653275228:web:881c5eb096d99288f09099',
 };
-
+ 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const storage = getStorage(app);
